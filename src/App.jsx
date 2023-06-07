bb7865161dba960b0c521d7d1bed960f294638c7
ishukhan <ishukk00@gmail.com>
2023-06-07 17:57:29 +0530

    all Routes using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Single from "./pages/single/Single";
 import New from "./pages/new/New";
 function App() {
   return (
-    <>
+    <div className="app">
       <BrowserRouter>
         <Routes>
           <Route path="/">
@@ -15,12 +15,17 @@ function App() {
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
-              <Route path=":new" element={<New />} />
+              <Route path="new" element={<New />} />
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route path="new" element={<New />} />
             </Route>
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
