9e5ab08e03cf5ef7b3848f2bfbc63f274e60a31e
ishukhan <ishukk00@gmail.com>
2023-06-08 15:12:11 +0530

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,51 @@
 import React from "react";
 import "./navbar.scss";
+
+import SearchIcon from "@mui/icons-material/Search";
+import LanguageIcon from "@mui/icons-material/Language";
+import NightsStayOutlinedIcon from "@mui/icons-material/NightsStayOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageIcon className="icon" />
+            English
+          </div>
+          <div className="item">
+            <NightsStayOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsOutlinedIcon className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <img
+              src="https://i.ibb.co/tZcqPFp/DSC-0040-removebg-preview-1.png"
+              alt="user profile"
+              className="avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
