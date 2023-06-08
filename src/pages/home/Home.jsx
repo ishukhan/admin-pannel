a660e35cfcbc92d2d9e010e600a7d6ea61652533
ishukhan <ishukk00@gmail.com>
2023-06-08 12:40:12 +0530

    home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./home.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className="homeContainer">Container</div>
+      <div className="homeContainer">
+        <Navbar />
+        home Container
+      </div>
     </div>
   );
 };
