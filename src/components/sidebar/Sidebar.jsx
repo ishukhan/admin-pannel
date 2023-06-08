3b5df937f0719f6390f04191eb1d57420af03585
ishukhan <ishukk00@gmail.com>
2023-06-08 11:09:10 +0530

    sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 import "./sidebar.scss";
 const Sidebar = () => {
-  return <div className="sidebar">sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">Ishuadmin</span>
+      </div>
+      <div className="center">
+        <ul>
+          <li>
+            <span>DashBord</span>
+          </li>
+          <li>
+            <span>DashBord</span>
+          </li>
+          <li>
+            <span>DashBord</span>
+          </li>
+          <li>
+            <span>DashBord</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">Color option</div>
+    </div>
+  );
 };
 
 export default Sidebar;
