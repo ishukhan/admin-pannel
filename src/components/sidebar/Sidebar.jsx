3dfb22c951d0adee05b230042356c48c5d6593f0
ishukhan <ishukk00@gmail.com>
2023-06-08 12:35:18 +0530

    sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,28 +1,87 @@
 import React from "react";
 import "./sidebar.scss";
+
+import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
+import PersonIcon from "@mui/icons-material/Person";
+import StoreIcon from "@mui/icons-material/Store";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import InsertChartIcon from "@mui/icons-material/InsertChart";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import HealthAndSafetyOutlinedIcon from "@mui/icons-material/HealthAndSafetyOutlined";
+import PsychologyIcon from "@mui/icons-material/Psychology";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LogoutIcon from "@mui/icons-material/Logout";
+
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="top">
         <span className="logo">Ishuadmin</span>
       </div>
+      <hr />
       <div className="center">
         <ul>
+          <p className="title">MAIN</p>
           <li>
+            <SpaceDashboardIcon className="icon" />
             <span>DashBord</span>
           </li>
+          <p className="title">LISTS</p>
           <li>
-            <span>DashBord</span>
+            <PersonIcon className="icon" />
+            <span>Users</span>
           </li>
           <li>
-            <span>DashBord</span>
+            <StoreIcon className="icon" />
+            <span>Products</span>
           </li>
           <li>
-            <span>DashBord</span>
+            <CreditCardIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <InsertChartIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <HealthAndSafetyOutlinedIcon className="icon" />
+            <span>System Helath</span>
+          </li>
+          <li>
+            <PsychologyIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon className="icon" />
+            <span>Setting</span>
+          </li>
+          <p className="title">USERS</p>
+          <li>
+            <AccountCircleIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutIcon className="icon" />
+            <span>Logout</span>
           </li>
         </ul>
       </div>
-      <div className="bottom">Color option</div>
+      <div className="bottom">
+        <div className="colorOptions"></div>
+        <div className="colorOptions"></div>
+      </div>
     </div>
   );
 };
