d2a0b8cc121d2bbf2d1eba7ff59cb9895781a1ac
ishukhan <ishukk00@gmail.com>
2023-06-09 10:38:59 +0530

    Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import "./home.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import Widget from "../../components/widget/Widget";
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-        home Container
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
       </div>
     </div>
   );
