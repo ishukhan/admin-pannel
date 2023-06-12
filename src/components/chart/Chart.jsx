ef9c1c22353b0471554b449455e7804465731bbc
ishukhan <ishukk00@gmail.com>
2023-06-12 19:38:02 +0530

    Chart Component

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,55 @@
+import "./chart.scss";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  { name: "Jan", Total: "12000" },
+  { name: "Feb", Total: "20000" },
+  { name: "Mar", Total: "82000" },
+  { name: "Apr", Total: "100000" },
+  { name: "May", Total: "16000" },
+  { name: "Jun", Total: "30000" },
+];
+
+const Chart = () => {
+  return (
+    <div className="chart">
+      <h1 className="title">Total 6 month (Revenue)</h1>
+      <ResponsiveContainer width="100%" aspect={2 / 1}>
+        <AreaChart
+          width={730}
+          height={250}
+          data={data}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="Total"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#total)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default Chart;
