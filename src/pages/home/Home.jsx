deecb8463482f7c5a487e83b3f68a6fb64b6195d
ishukhan <ishukk00@gmail.com>
2023-06-12 20:06:46 +0530

    Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
 import Featured from "../../components/featured/Featured";
 import Chart from "../../components/chart/Chart";
+import Table from "../../components/table/Table";
 const Home = () => {
   return (
     <div className="home">
@@ -20,6 +21,10 @@ const Home = () => {
         <div className="charts">
           <Featured />
           <Chart />
+        </div>
+        <div className="listContainer">
+          <h1 className="listTitle">Latest Transaction</h1>
+          <Table />
         </div>
       </div>
     </div>
