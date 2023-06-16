e19079169d60fd3334e395d7dca74716b739d513
ishukhan <ishukk00@gmail.com>
2023-06-16 09:02:03 +0530

    Single user page

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "./single.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 const Single = () => {
-  return <div>single</div>;
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+      </div>
+    </div>
+  );
 };
 
 export default Single;
