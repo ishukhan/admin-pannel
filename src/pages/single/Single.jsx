618370bf764a2940db1a0a874eb33fbcfae05d5d
ishukhan <ishukk00@gmail.com>
2023-06-16 10:14:50 +0530

    Single user page

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -2,12 +2,55 @@ import React from "react";
 import "./single.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import Chart from "../../components/chart/Chart";
+import List from "../../components/table/Table";
 const Single = () => {
   return (
     <div className="single">
       <Sidebar />
       <div className="singleContainer">
         <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://i.ibb.co/tZcqPFp/DSC-0040-removebg-preview-1.png"
+                alt="profile"
+                className="itemImg"
+              />
+              <div className="deatil">
+                <h1 className="itemTitle">Ishu khan</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">ishukk00@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">+91- 7667049362</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">
+                    Sector 2 noida India, 847045
+                  </span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">India</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3 / 1} title="User spend (Last 6 month)" />
+          </div>
+        </div>
+        <div className="bottom">
+          <h1 className="title">Last Transaction</h1>
+          <List />
+        </div>
       </div>
     </div>
   );
