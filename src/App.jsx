ce898cf3e8ed7558df9fac36a12ad02689bca777
ishukhan <ishukk00@gmail.com>
2023-06-19 18:56:13 +0530

    all Routes using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
+import { productInputs, userInputs } from "./formSorce";
 function App() {
   return (
     <div className="app">
@@ -15,12 +16,18 @@ function App() {
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route
+                path="new"
+                element={<New inputs={userInputs} title="Add New User" />}
+              />
             </Route>
             <Route path="products">
               <Route index element={<List />} />
               <Route path=":productId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route
+                path="new"
+                element={<New inputs={productInputs} title="Add New Product" />}
+              />
             </Route>
           </Route>
         </Routes>
