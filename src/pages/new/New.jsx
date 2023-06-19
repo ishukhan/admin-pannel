a462c45cd86ec0dd831549a95c932215915d8f23
ishukhan <ishukk00@gmail.com>
2023-06-19 14:03:19 +0530

    add new user oage

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import "./new.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 const New = () => {
-  return <div>new</div>;
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1 className="title">Add New User</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default New;
