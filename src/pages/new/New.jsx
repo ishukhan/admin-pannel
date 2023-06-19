4e270ca36c51e02da626766ca4ef93ff0b647746
ishukhan <ishukk00@gmail.com>
2023-06-19 18:58:45 +0530

    add new user page

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./new.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
-const New = () => {
+import { DriveFolderUploadOutlined } from "@mui/icons-material";
+const New = ({ inputs, title }) => {
+  const [file, setFile] = useState("");
+  console.log(file);
   return (
     <div className="new">
       <Sidebar />
       <div className="newContainer">
         <Navbar />
         <div className="top">
-          <h1 className="title">Add New User</h1>
+          <h1>{title}</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src="https://i.ibb.co/SvWhFN4/no-image-icon-0.jpg"
+              alt="addimg"
+              onChange={(e) => setFile(e.target.files[0])}
+              className="image"
+            />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadOutlined className="icon" />
+                </label>
+                <input type="file" id="file" hidden />
+              </div>
+              {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label>{input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
+                </div>
+              ))}
+
+              <button>Send</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
